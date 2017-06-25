c1f23dacb326b328033e1c1fdf83303aab8d1b9c
Amaar Esufally <amaar.esufally@gmail.com>
2017-06-24 22:01:01 -0400

    got some playbar functionality

--- frontend/components/bottomplaybar.jsx
+++ frontend/components/bottomplaybar.jsx
@@ -11,7 +11,14 @@ class BottomPlayBar extends React.Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
-
+    this.movebar = this.movebar.bind(this);
+    this.clickbar = this.clickbar.bind(this);
+    this.dragbar = this.dragbar.bind(this);
+    this.dragdrop = this.dragdrop.bind(this);
+    this.dragEnter = this.dragEnter.bind(this);
+    this.dragOver = this.dragOver.bind(this);
+    this.dragwidth = null;
+    this.dragmusic = null;
   }
 
   componentWillReceiveProps(nextProps){
@@ -26,9 +33,79 @@ class BottomPlayBar extends React.Component {
   }
 
   // componentDidUpdate() AT SOME POINT WE WANT TO CLEAR ALL THE USERS UP FROM THE STATE
-  componentDidMount(){
+
+
+  movebar(e){
+    let duration = this.music.duration;
+    let currentTime = this.music.currentTime;
+
+    let newval = currentTime/duration;
+    let res = newval.toLocaleString("en", {minimumFractionDigits: 10, style: "percent"})
+    this.playbar.style.width = res;
+  }
+
+  // We need to use this.clientX to get the X coordainate
+  // Can use clientwidth to set the width
+  // e.currentTarget.offsetleft ?
+  // How to get the actual position of the thing.
+  // Clientleft?
+  // e.currentTarget.clientLeft to get the
+  // e.target.offsetLeft + e.target.offsetParent.offsetLeft + e.target.offsetWidth
+
+  clickbar(e){
+    var holderwidth =  this.playbarholder.clientWidth
+
+    let duration = this.music.duration
+    var newpos = e.clientX - this.playbar.offsetLeft - this.playbar.offsetParent.offsetLeft;
+    this.ball.offsetLeft = newpos;
+    this.music.style.width = newpos;
+    this.music.currentTime = (newpos/holderwidth) * duration;
+    // debugger
 
   }
+
+
+  dragOver(e) {
+    e.preventDefault()
+    console.log("dragging over")
+       return false;
+    }
+
+
+  dragStart(e){
+    e.dataTransfer.effectAllowed='move';
+    e.dataTransfer.setData("Text", ev.target.getAttribute('id'));
+    ev.dataTransfer.setDragImage(ev.target,0,0);
+  }
+
+
+  dragbar(e){
+    // e.preventDefault();
+    var holderwidth =  this.playbarholder.clientWidth
+    console.log("hello");
+    let duration = this.music.duration
+
+    var newpos = e.clientX - this.playbar.offsetLeft - this.playbar.offsetParent.offsetLeft;
+    this.dragwidth = newpos;
+    this.dragmusic = (newpos/holderwidth) * duration;
+    // debugger
+  }
+
+  dragdrop(e){
+    // e.preventDefault();
+    debugger
+    console.log("drop")
+    this.playbar.style.width = this.dragwidth;
+    this.music.currentTime = this.dragmusic;
+  }
+
+  dragEnter(e) {
+    e.preventDefault();
+    console.log("can put")
+    return true;
+  }
+
+
 
 
   handleClick(){
@@ -40,7 +117,6 @@ class BottomPlayBar extends React.Component {
       this.music.pause();
 		  playbutton.className = "";
 		  playbutton.className = "play";
-      // debugger
     };
   }
 
@@ -53,7 +129,15 @@ class BottomPlayBar extends React.Component {
   // // adjust the CSS for the class?
   // Can check the duration of the player by doing this.audio.duration
   // Can check the current time by doing this.audio.currentTime
-  //
+    // Can check if the track has ended by doing this.audio.ended -- when we do this we will want to move on the next song in the playlist.
+    // this.audio.player - this will give us the amount of time that has been played in the song
+    // Maybe can do width of the bar = duration/played
+    // .timeupdate
+    // ontimeupdate can be used to fire off the event.
+    //1. make a bar which is has a set length (100%)
+    // Need to add on to the width through our ref, like -- playbar.width += (valuedivision);
+    //2. onclick of the line, we change the duration of the track to whatever we need.
+    //
   // }
 
 
@@ -63,15 +147,16 @@ class BottomPlayBar extends React.Component {
     const artist = this.props.artist
     if (this.props.audio.track_url !== "") {
       audioplayer = <div className="playbar">
-                          <audio ref={audio => this.music = audio} autoPlay>
+                          <audio onTimeUpdate={this.movebar}  ref={audio => this.music = audio} autoPlay >
                             <source src={this.props.audio.track_url}type="audio/ogg" />
                             <source src={this.props.audio.track_url} type="audio/mpeg" />
                           </audio>
                         <div className="controls">
                         <button id="playbutton" className="pause" onClick={this.handleClick} />
                         </div>
-                        <div className="progress">
-                          <div ref={div => this.playbar = div}>Placeholder For Progress bar</div>
+                        <div className="progress-bar-background" ref={div => this.playbarholder = div} onClick={this.clickbar} onDrop={this.dragdrop} onDragEnter={this.dragEnter} onDragOver={this.dragOver}>
+                          <div className="dragball" ref={div => this.ball = div} onDragStart={this.dragbar} onDrag={this.dragbar}  draggable="true" ></div>
+                          <div className="progress-bar" ref={div => this.playbar = div} onClick={this.clickbar} ></div>
                         </div>
                           <div className="currentSongInfo"> <img  className="song-coverart-playerslice" src={this.props.audio.cover_art_url} />
                           <div className="song-infoplayer-slice">
@@ -95,7 +180,6 @@ const selectSingleArtist = (state) => {
 }
 
 const mapStateToProps = (state) => {
-  debugger
   return { audio: state.audio,
   artist: selectSingleArtist(state) }
 }
