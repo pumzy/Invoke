fcd342fd80694a953b6d557fcb16ab5f8e91ed71
Amaar Esufally <amaar.esufally@gmail.com>
2017-06-29 13:50:22 -0400

    test push

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
-        test: require.resolve("wavesurfer.js"),
-        loader: "expose?WaveSurfer",
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
