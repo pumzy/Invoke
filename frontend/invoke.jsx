5c277fc238fee802912fc6652011753a1490ad33
Amaar Esufally <amaar.esufally@gmail.com>
2017-06-29 12:32:58 -0400

    added a comma

--- frontend/invoke.jsx
+++ frontend/invoke.jsx
@@ -16,11 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  window.fetchSongByUserID = fetchSongByUserID
-  window.fetchSongByTitle = fetchSongByTitle
-  window.deleteSong = deleteSong
-  window.store = store
+  // 
+  // window.fetchSongByUserID = fetchSongByUserID
+  // window.fetchSongByTitle = fetchSongByTitle
+  // window.deleteSong = deleteSong
+  // window.store = store
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
