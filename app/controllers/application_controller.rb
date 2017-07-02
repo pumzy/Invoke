95a4359d398665dea3bb0c70ea186a5a3fdb2f0e
Amaar Esufally <amaar.esufally@gmail.com>
2017-07-02 19:02:09 -0400

    ewgw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
   helper_method :current_user, :logged_in?
 
   private
